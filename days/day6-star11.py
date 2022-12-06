c4ea92331dc2eb80a4650db592e8de4933a911d1
Daniel Kroese <djikoloz@hotmail.com>
2022-12-07 00:55:26 +0100

    integrate if i larger then

--- days/day6-star11.py
+++ days/day6-star11.py
@@ -3,9 +3,8 @@
 relative_path = './inputs/input-day6-2022'
 with open(relative_path,"r") as file_handle:
     line = file_handle.read()
-    for i in range(0,len(line)):
-        if i > 3: 
-            checklist = line[i-4:i]
-            if len(checklist) == len(set(checklist)):
-                print("all unique values",checklist,"at value",i)
-                break
+    for i in range(4,len(line)): 
+        checklist = line[i-4:i]
+        if len(checklist) == len(set(checklist)):
+            print("all unique values",checklist,"at value",i)
+            break
--- days/day6-star12.py
+++ days/day6-star12.py
@@ -3,9 +3,8 @@
 relative_path = './inputs/input-day6-2022'
 with open(relative_path,"r") as file_handle:
     line = file_handle.read()
-    for i in range(0,len(line)):
-        if i > 13: 
-            checklist = line[i-14:i]
-            if len(checklist) == len(set(checklist)):
-                print("all unique values",checklist,"at value",i)
-                break
+    for i in range(14,len(line)): 
+        checklist = line[i-14:i]
+        if len(checklist) == len(set(checklist)):
+            print("all unique values",checklist,"at value",i)
+            break
