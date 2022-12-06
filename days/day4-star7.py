7053bd81d457a7c5b6ec5fb28dbd53bdc9219c87
Daniel Kroese <djikoloz@hotmail.com>
2022-12-06 16:09:02 +0100

    day4 part1

---
+++ days/day4-star7.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env/python3
+occurances = 0
+relative_path = './inputs/input-day4-2022'
+with open(relative_path,"r") as file_handle:
+    lines_list = file_handle.readlines()
+    print(len(lines_list)," Lines counted")
+    totalScore = 0
+    for i in range(0,len(lines_list)): 
+        print(lines_list[i])
+        range1 = lines_list[i].split(",")[0].rstrip()
+        startrange1 = range1.split("-")[0]
+        endrange1 = range1.split("-")[1]
+        range2 = lines_list[i].split(",")[1].rstrip()
+        startrange2 = range2.split("-")[0]
+        endrange2 = range2.split("-")[1]
+        print("Range1:",startrange1,endrange1,"Range2:",startrange2,endrange2)
+        if(int(startrange1) >= int(startrange2) and int(endrange1) <= int(endrange2)):
+            print("Range1 valt binnen Range2")
+            occurances += 1
+        elif(int(startrange1) <= int(startrange2) and int(endrange1) >= int(endrange2)):
+            print("Range2 valt binnen Range1")
+            occurances += 1
+print("# Totalscore: ",occurances)
